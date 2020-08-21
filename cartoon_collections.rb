b3355fd70debdfcb6f4eb88b74e63b65ede5b2d2
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-21 07:04:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,6 +24,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.detect do |calls|
     if valid_calls.include?(calls)
+      calls
     end
   end
 end
